f156ea9f3d8787e21ff99e2150ef951878074d62
Thomas Kam <thomassth@users.noreply.github.com>
2025-06-15 00:17:03 +0000

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://astronaut.github.io',
   experimental: {
     headingIdCompat: true,
   },
